a32f7c783f13daa7d9e40a4798c6eed7ef085331
Josephine Salamon <jsalamon@lcs.on.ca>
2025-02-27 09:58:51 -0500

    Initial Commit

---
+++ DiscountCalculator/DiscountCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscountCalculatorApp.swift
+//  DiscountCalculator
+//
+//  Created by Josephine Salamon on 2025-02-27.
+//
+
+import SwiftUI
+
+@main
+struct DiscountCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DiscountCalculatorTests/DiscountCalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  DiscountCalculatorTests.swift
+//  DiscountCalculatorTests
+//
+//  Created by Josephine Salamon on 2025-02-27.
+//
+
+import Testing
+@testable import DiscountCalculator
+
+struct DiscountCalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
